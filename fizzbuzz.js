69390224589746a03a4fce80378252f9057b2561
mikesmall <michael.l.a.small@gmail.com>
2017-12-22 13:31:59 -0500

    Changed 'this' to 'theNumber' in functions itsFizz, itsBuzz, itsFizzBuzz, itsJustaDud

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,26 +7,26 @@ document.addEventListener('DOMContentLoaded', function() {
     this.style.fontWeight = '400';
   }
   function itsFizz() {
-    this.innerText = 'fizz';
+    number.innerText = 'fizz';
     console.log('The square should say fizz.');
     bingo();
     console.log('The square should be red.');
   };
   function itsBuzz() {
-    this.innerText = 'buzz';
+    number.innerText = 'buzz';
     console.log('The square should say buzz.');
     bingo();
     console.log('The square should be red.');
   };
   function itsFizzBuzz() {
-    this.innerText = 'fizzbuzz';
+    number.innerText = 'fizzbuzz';
     console.log('The square should say fizzbuzz.');
     bingo();
     console.log('The square should be red.');
   };
   function itsJustaDud() {
     console.log('The square text should not change.');
-    this.style.backgroundColor = 'lightgrey';
+    number.style.backgroundColor = 'lightgrey';
     console.log('The square should be grey.');
   };
 
